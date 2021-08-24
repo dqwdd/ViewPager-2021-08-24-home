55333b3fd1128bc9856ae7999eca05ede16a4abe
dqwdd <dqwdd@naver.com>
2021-08-24 10:54:04 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/example/viewpager_2021_08_24_home/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.viewpager_2021_08_24_home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
