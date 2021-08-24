c9680c26efa18272e827e3640e42a94528189876
dqwdd <dqwdd@naver.com>
2021-08-24 11:17:57 +0900

    뷰페이저 쓸 어댑터 작성

---
+++ app/src/main/java/com/example/viewpager_2021_08_24_home/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,34 @@
+package com.example.viewpager_2021_08_24_home.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.example.viewpager_2021_08_24_home.fragments.BirthYearFragment
+import com.example.viewpager_2021_08_24_home.fragments.HelloFragment
+import com.example.viewpager_2021_08_24_home.fragments.NameFragment
+
+class MainViewPagerAdapter( fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
+//        TODO("Not yet implemented")
+//        뷰페이저가 총 몇장인지(겟카운트니까)
+//        return > 우린 3장이니 3
+        return 3
+
+    }
+//TODO주석 있는 상태로 앱 키면 죽으니까 지워줘야 함
+    override fun getItem(position: Int): Fragment {
+//        TODO("Not yet implemented")
+//        각각의 position에 어떤 Fragment가 배치되어야 하는가?
+        if (position==0) {
+            return HelloFragment()
+        }
+    if (position==1) {
+        return NameFragment()
+    }
+
+    else {
+        return BirthYearFragment()
+    }
+    }
+
+}
