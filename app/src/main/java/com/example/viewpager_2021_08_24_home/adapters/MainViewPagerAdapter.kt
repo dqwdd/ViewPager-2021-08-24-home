ce983303975f1dd84bf9bbaf92b58cbb04c7a429
dqwdd <dqwdd@naver.com>
2021-08-24 11:50:24 +0900

    페이지별 제목 부여 작업

--- app/src/main/java/com/example/viewpager_2021_08_24_home/adapters/MainViewPagerAdapter.kt
+++ app/src/main/java/com/example/viewpager_2021_08_24_home/adapters/MainViewPagerAdapter.kt
@@ -8,6 +8,23 @@ import com.example.viewpager_2021_08_24_home.fragments.HelloFragment
 import com.example.viewpager_2021_08_24_home.fragments.NameFragment
 
 class MainViewPagerAdapter( fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+    override fun getPageTitle(position: Int): CharSequence? {
+//        CharSequence==String임
+        if (position==0) {
+            return "인사"
+        }
+        if (position==1) {
+            return "이름"
+        }
+        else {
+            return "출생년도"
+        }
+    }
+
+
+
+
     override fun getCount(): Int {
 //        TODO("Not yet implemented")
 //        뷰페이저가 총 몇장인지(겟카운트니까)
