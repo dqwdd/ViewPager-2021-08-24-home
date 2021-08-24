58a6faf2e2f3c3ebc319038064840fc33f07d5f1
dqwdd <dqwdd@naver.com>
2021-08-24 11:03:16 +0900

    HelloFragment레이아웃과 클래스 생성

---
+++ app/src/main/java/com/example/viewpager_2021_08_24_home/fragments/HelloFragment.kt
@@ -0,0 +1,27 @@
+package com.example.viewpager_2021_08_24_home.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewpager_2021_08_24_home.R
+
+class HelloFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(
+            R.layout.fragment_hello, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+//        이건 코드 딱히 x
+    }
+
+}
